542a7a94ff69723d609cadcf985f8f481e1bdd32
-has1g1r- <has1g15@soton.ac.uk>
2017-07-13 16:07:15 +0100

    change arbitrary users passwords

--- expressapp/routes/changepassword.js
+++ expressapp/routes/changepassword.js
@@ -1,16 +1,76 @@
 var express = require('express');
 var router = express.Router();
 
+var stuff;
+var users = require('./users.js')
+var parts = "";
+var exec = require('child_process').exec;
+var child;
+var glob = require('../app.js');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('changepassword', { title: 'Change Your Password', name: 'user' });
+
+
+var usernames = [];
+var count = 0; 
+
+child = exec("aws dynamodb scan --table-name UserDetailsDev", function (error, stdout, stderr) {
+	console.log("Scanning table");
+		
+			if(error !== null) {
+				console.log("Error in forgot password" + stderr);
+			}
+			stuff = stdout;
+			parts = stuff.split('\"');
+	  //goes through the usernames in DynamoDB and checks if they're in Cognito or not, if not it adds them		
+			for (var i = 21; i < parts.length; i+=24)
+			{
+				usernames[count] = parts[i];
+				console.log(usernames[count]);
+					 count++;
+			}
+			
+			if (error !== null) {
+				console.log('exec error: ' + error);
+			}
+			
+			rand = Math.round(random(0, count -1));
+			console.log(rand);
+			console.log("Simulating " + usernames[rand] + " changing password");
+		
+		child=exec("aws cognito-idp forgot-password --client-id 5q5hugosg9t383rpi5mcfn0j74 --username " + usernames[rand], function (error, stdout, stderr){ });
+			//When incorportating front end features, this would be the command used, there would be facility for the user to enter the confirmation code they have received via email
+			/*child=exec("aws cognito-idp confirm-forgot-password --client-id 5q5hugosg9t383rpi5mcfn0j74 --confirmation-code " + valcode + " --password " + pass + " --username " + uname, function (error, stdout, stderr){
+				if (error == null)
+				{
+					console.log('Your password has been successfully reset');
+					action = "Password Reset Successfully";
+				} else {
+					console.log("Error: " + stderr);
+				}
+			});*/
+			child=exec("aws cognito-idp admin-initiate-auth --user-pool-id us-east-2_M8LZIsbAN --client-id 5q5hugosg9t383rpi5mcfn0j74 --auth-flow ADMIN_NO_SRP_AUTH --auth-parameters USERNAME=" + usernames[rand] + ",PASSWORD=Password1", function (error, stdout, stderr){
+				if (error !== null) {
+					console.log("Error: " + stderr);
+				}
+				
+				var token = stdout.split('\"');
+				child=exec("aws cognito-idp change-password --previous-password Password1 --proposed-password Password1 --access-token " + token[19], function (error, stdout, stderr){
+					if (error == null) {
+						console.log('Password Successfully Changed!');
+						action = "Password changed";
+					} else {
+						console.log("Error: " + stderr);
+					}
+				});		
+			});
+			router.get('/', function(req, res, next) {
+			  res.render('changepassword', { title: 'Change Your Password', username: usernames[rand] });
+			});
 });
-var answer;
-var answer2;
-var answer3;
-const readline = require('readline');
-var uname;
-var pass;
-var prepass;
+
+function random (low, high) {
+    return Math.random() * (high - low) + low;
+}
 
 module.exports = router;
--- expressapp/routes/users.js
+++ expressapp/routes/users.js
@@ -16,7 +16,7 @@ router.get('/', function(req, res, next) {
   res.render('users', { title: 'Users', name: 'user' });
 });
 
-const readline = require('readline');
+/*const readline = require('readline');
 const rl = readline.createInterface({
 	input: process.stdin,
     output: process.stdout
@@ -34,13 +34,14 @@ rl.question('Run config: \n 1 - Development \n 2 - Production \n', (answer1) => 
 		clientID = "6m4hcbg93084uktp7bep6m8128";
 		tablename = "UserDetailsProd";
 		poolID = "us-east-2_6Ty3PX9Ss";
-	}
-	child = exec("aws dynamodb scan --table-name " + tablename, function (error, stdout, stderr) {
-		child = exec("aws cognito-idp list-users --user-pool-id " + poolID, function (error2, stdout2, stderr2) {
+	}*/
+	child = exec("aws dynamodb scan --table-name UserDetailsDev", function (error, stdout, stderr) {
+		child = exec("aws cognito-idp list-users --user-pool-id us-east-2_M8LZIsbAN", function (error2, stdout2, stderr2) {
 			userPool = stdout2;
 			if(error !== null) {
 				console.log("I am an error" + stderr2);
 			}
+			
 			stuff = stdout;
 			parts = stuff.split('\"');
 	  //goes through the usernames in DynamoDB and checks if they're in Cognito or not, if not it adds them		
@@ -50,7 +51,7 @@ rl.question('Run config: \n 1 - Development \n 2 - Production \n', (answer1) => 
 				if (userPool.indexOf(parts[i]) < 0)
 				{
 					console.log(parts[i] + " has been checked");
-					child=exec("aws cognito-idp sign-up --client-id " + clientID + " --password Password5 --user-attributes Name=email,Value=" + parts[i+6] + " Name=phone_number,Value=+447543216789 --username " + parts[i] + " --region us-east-2", function (error, stdout, stderr){
+					child=exec("aws cognito-idp sign-up --client-id 5q5hugosg9t383rpi5mcfn0j74 --password Password1 --user-attributes Name=email,Value=" + parts[i+6] + " Name=phone_number,Value=+447543216789 --username " + parts[i] + " --region us-east-2", function (error, stdout, stderr){
 						if (error == null)
 						{
 							console.log("User Successfully Created");
@@ -62,13 +63,14 @@ rl.question('Run config: \n 1 - Development \n 2 - Production \n', (answer1) => 
 					});
 				}	 
 			}
+			
 			if (error !== null) {
 				console.log('exec error: ' + error);
 			}
 		});
 	});
-});
 //scanning the users in our DynamoDB and adding ones to Cognito if they aren't already there
 
-module.exports = router;
 
+
+module.exports = router;
